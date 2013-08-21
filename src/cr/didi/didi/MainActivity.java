05473d9a47ae79c17375155741c04a1904633d09
Gustavo Ramírez Hidalgo <tavroot@gmail.com>
2013-08-21 17:34:53 -0600

    validaciones al obtener el value del edit text y utilizarlo con el handler de productos

--- src/cr/didi/didi/MainActivity.java
+++ src/cr/didi/didi/MainActivity.java
@@ -11,14 +11,16 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	public final static String EXTRA_MESSAGE = "cr.didi.didi.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +40,63 @@ public class MainActivity extends Activity {
     public void haciaLaBusqueda(View view) {
     	
     	//System.out.print("La cosa.");
+    	String message=null;
+    	
+    	//Por que no sirven estos try, catch e if??
+    	//FALTA VALIDAR EL CASO DE ESTAR VACIO EL EDItTEXT
+    	try{
+        	EditText editText = (EditText) findViewById(R.id.text_field_busqueda_inicio);
+        	message = editText.getText().toString();
+    	}
+    	catch(Exception e){
+    		message="0";
+    	}
+    	if(message == ""){
+    		Toast.makeText(MainActivity.this, "ajua", Toast.LENGTH_SHORT).show();
+    		message="0";
+    	}
+    	
+    	Toast.makeText(MainActivity.this, "sin error...", Toast.LENGTH_SHORT).show();
+    	//Toast.makeText(MainActivity.this, "http://tecmo.webfactional.com/didi/productos?idClienteParam="+message, Toast.LENGTH_SHORT).show();
+
+    	DefaultHttpClient   httpclient = new DefaultHttpClient(new BasicHttpParams());
+    	HttpGet httpget = new HttpGet("http://tecmo.webfactional.com/didi/productos?idClienteParam="+message);
+    	// Depends on your web service
+    	httpget.setHeader("Content-type", "application/json");
+    	InputStream inputStream = null;
+    	String result = null;
+    	try {
+    	    HttpResponse response = httpclient.execute(httpget);
+    		//Toast.makeText(MainActivity.this, "sin error", Toast.LENGTH_SHORT).show();
+    	    HttpEntity entity = response.getEntity();
+
+    	    inputStream = entity.getContent();
+    	    // json is UTF-8 by default
+    	    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+    	    StringBuilder sb = new StringBuilder();
+
+    	    String line = null;
+    	    while ((line = reader.readLine()) != null)
+    	    {
+    	        sb.append(line + "\n");
+    	    }
+    	    result = sb.toString();
+    	    Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+    	} catch (Exception e) {
+    		Toast.makeText(MainActivity.this, "error alguno...", Toast.LENGTH_SHORT).show();
+    	    // Oops
+    	}
+    	finally {
+    		//Toast.makeText(MainActivity.this, "entro a finally...", Toast.LENGTH_SHORT).show();
+    	    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+    	}
+
+    	//Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	//string msg
+    	//intent.putExtra(EXTRA_MESSAGE, message);
     	
     	//Put up the Yes/No message box
+    	/**
     	AlertDialog.Builder builder = new AlertDialog.Builder(this);
     	builder.setTitle("Volarse toda la info de su cel");
     	builder.setMessage("Seguro?");
@@ -48,44 +105,13 @@ public class MainActivity extends Activity {
     	    public void onClick(DialogInterface dialog, int which) {
     	    	//Yes button clicked, do something
     	    	
-    	    	DefaultHttpClient   httpclient = new DefaultHttpClient(new BasicHttpParams());
-    	    	HttpGet httpget = new HttpGet("http://tecmo.webfactional.com/didi/provincias");
-    	    	// Depends on your web service
-    	    	httpget.setHeader("Content-type", "application/json");
-    	    	InputStream inputStream = null;
-    	    	String result = null;
-    	    	try {
-    	    	    HttpResponse response = httpclient.execute(httpget);
-    	    		Toast.makeText(MainActivity.this, "sin error", Toast.LENGTH_SHORT).show();
-    	    	    HttpEntity entity = response.getEntity();
-
-    	    	    inputStream = entity.getContent();
-    	    	    // json is UTF-8 by default
-    	    	    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
-    	    	    StringBuilder sb = new StringBuilder();
-
-    	    	    String line = null;
-    	    	    while ((line = reader.readLine()) != null)
-    	    	    {
-    	    	    	Toast.makeText(MainActivity.this, line, Toast.LENGTH_SHORT).show();
-    	    	        sb.append(line + "\n");
-    	    	    }
-    	    	    result = sb.toString();
-    	    	} catch (Exception e) {
-    	    		Toast.makeText(MainActivity.this, "error alguno...", Toast.LENGTH_SHORT).show();
-    	    	    // Oops
-    	    	}
-    	    	finally {
-    	    		Toast.makeText(MainActivity.this, "entro a finally...", Toast.LENGTH_SHORT).show();
-    	    	    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
-    	    	}
     	    	
     	    	//Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
     	    }
     	});
     	builder.setNegativeButton("No", null);						//Do nothing on no
     	builder.show();
-    	
+    	*/
     	
     }
     
