0cf00fb9aa49dcfbbfe7a0826858b12856f4932f
Gustavo Ramírez Hidalgo <tavroot@gmail.com>
2013-09-25 14:58:05 -0600

    paso hacia el view de categorias (directorio), desde cualquier parte del menu principal

--- src/cr/didi/didi/EventosActivity.java
+++ src/cr/didi/didi/EventosActivity.java
@@ -27,6 +27,7 @@ import android.widget.Toast;
 public class EventosActivity extends Activity {
 	
 	public final static String EXTRA_MESSAGE_RESULT_SEARCH = "cr.didi.didi.MESSAGE";
+	public final static String EXTRA_MESSAGE_CAT_REQUEST = "cr.didi.didi.MESSAGE_CAT_REQUEST";
 	private static float init_x = 0;
 
 	@Override
@@ -131,6 +132,74 @@ public class EventosActivity extends Activity {
     	}
     }
     
+    private class MyAsyncTask2 extends AsyncTask<String, Integer, Double>{
+    	@Override
+    	protected Double doInBackground(String... params) {
+    		// TODO Auto-generated method stub
+    	    requestCategorias();
+    	    return null;
+    	}
+    	protected void onPostExecute(Double result){
+    	    ProgressBar pb=(ProgressBar)findViewById(R.id.progressBar1);
+    		pb.setVisibility(View.GONE);
+    		Toast.makeText(getApplicationContext(), "Listo", Toast.LENGTH_LONG).show();
+    	}
+    	protected void onProgressUpdate(Integer... progress){
+    	    ProgressBar pb=(ProgressBar)findViewById(R.id.progressBar1);
+    	    pb.setProgress(progress[0]);
+    	}
+    }
+
+    //Ejecucion que se le asigna al boton de Eventos para el menu principal
+    public void clickeadoAlBotonDirectorio(View view) {
+    	//888888888888888888888888888888888888888888888888888888888888888888888888
+		ProgressBar pb=(ProgressBar)findViewById(R.id.progressBar1);
+		pb.setVisibility(View.VISIBLE);
+		new MyAsyncTask2().execute();
+    	//888888888888888888888888888888888888888888888888888888888888888888888888
+    }
+    
+    //Request de categorias
+    public void requestCategorias() {
+    	DefaultHttpClient   httpclient = new DefaultHttpClient(new BasicHttpParams());
+    	HttpGet httpget = new HttpGet("http://tecmo.webfactional.com/didi/categorias");
+    	//Depends on your web service
+    	httpget.setHeader("Content-type", "application/json");
+    	InputStream inputStream = null;
+    	String result = null;
+    	try {
+    	    HttpResponse response = httpclient.execute(httpget);
+    		//Toast.makeText(MainActivity.this, "sin error", Toast.LENGTH_SHORT).show();
+    	    HttpEntity entity = response.getEntity();
+
+    	    inputStream = entity.getContent();
+    	    // json is UTF-8 by default
+    	    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+    	    StringBuilder sb = new StringBuilder();
+
+    	    String line = null;
+    	    while ((line = reader.readLine()) != null)
+    	    {
+    	        sb.append(line + "\n");
+    	    }
+    	    result = sb.toString();
+    	    //Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+    	} catch (Exception e) {
+    		Toast.makeText(EventosActivity.this, "error alguno...", Toast.LENGTH_SHORT).show();
+    	    // Oops
+    	}
+    	finally {
+    		//Toast.makeText(MainActivity.this, "entro a finally...", Toast.LENGTH_SHORT).show();
+    	    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+    	}
+    	Intent intent = new Intent(this, CategoriasActivity.class);
+    	//EditText editText = (EditText) findViewById(R.id.edit_message);
+    	//String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE_CAT_REQUEST, result);
+    	startActivity(intent);
+    }
+
+    
     
     public void clickeadoAlBotonBusqueda(View view) {
     	String value=null;
@@ -169,16 +238,6 @@ public class EventosActivity extends Activity {
     	startActivity(intent);
     }
     
-    //Ejecucion que se le asigna al boton de Eventos para el menu principal
-    public void clickeadoAlBotonDirectorio(View view) {
-    	Intent intent = new Intent(this, CategoriasActivity.class);
-    	//EditText editText = (EditText) findViewById(R.id.edit_message);
-    	//String message = editText.getText().toString();
-    	//intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
-    }
-	
-	
 
 	/**
 	 * Set up the {@link android.app.ActionBar}, if the API is available.
